b0a556def83aa6b6532e85f728c742517b6de8b1
John Cipriano <johnmikecip@gmail.com>
2014-04-23 17:24:18 -0700

    reformatted

--- bst.py
+++ bst.py
@@ -1,5 +1,5 @@
 """
-Binary tree stuff.
+Recursive binary search tree implementation (does not balance on add).
 """
 
 
@@ -20,19 +20,16 @@ class Node:
 
 class BST:
     """
-    An unbalanced binary tree.
+    A binary search tree.
     """
 
     def __init__(self, k):
         self.root = Node(k)
 
-    def add(self, k):
-        """
-        Adds an entry to the binary tree.
-        """
-        self._add(k, self.root)
-
     def _add(self, k, node):
+        """
+        Adds a key to the tree rooted at node.
+        """
         if not node:
             return Node(k)
         if k < node.key:
@@ -44,147 +41,27 @@ class BST:
         else:  # k == node.key
             return node
 
-    def inorder(self):
-        """
-        in-order traversal
-        """
-        return self._inorder(self.root, [])
-
-    def _inorder(self, node, q):
-        if node:
-            self._inorder(node.left, q)
-            q.append(node.key)
-            self._inorder(node.right, q)
-
-        return q
-
-    def _print_level(self, node, level):
-        if not node:
-            return ""
-        if level == 1:
-            return str(node.key) + " "
-        else:
-            left = self._print_level(node.left, level - 1)
-            right = self._print_level(node.right, level - 1)
-            return str(left) + str(right)
-
-    def level_order(self):
-        """
-        Level-order print.
-        """
-        maxh = self._height(self.root) + 1
-        levels = [self._print_level(self.root, level) for level in range(1, maxh)]
-        return "\n".join(levels)
-
-    def height(self):
-        """
-        Returns the max height of the binary tree instance.
-        """
-        return self._height(self.root)
-
-    def _height(self, node):
-        """
-        Returns the max height of the tree rooted at node.
-        """
-        if not node:
-            return 0
-
-        return 1 + max(self._height(node.left),
-                       self._height(node.right))
-
-    def floor(self, k):
-        """
-        Returns the largest key smaller than k.
-        """
-        return self._floor(self.root, k)
-
-    def _floor(self, node, k):
-        # search key is smaller: keep going left
-        # search key is larger: floor(right) if one exists, else self
-        if not node:
-            return None
-        elif k == node.key:
-            return k
-        elif k < node.key:
-            return self._floor(node.left, k)
-        elif k > node.key:
-            rf = self._floor(node.right, k)
-            return rf if rf else node.key
-
-    def ceil(self, k):
-        """
-        Returns the smallest key larger than k.
-        """
-        return self._ceil(self.root, k)
-
     def _ceil(self, node, k):
-        # search key is larger: keep going right
-        # search key is smaller: floor(left) if one exists, else self
+        """
+        Returns the smallest key larger than k from the subtree rooted at node.
+        """
         if not node:
             return None
 
         if k > node.key:
+            # key is larger: continue right
             return self._ceil(node.right, k)
         elif k < node.key:
+            # key is smaller: floor(left) if one exists, else cur
             lf = self._ceil(node.left, k)
             return lf if lf else node.key
         else:  # k == node.key
             return k
 
-    def get(self, k):
-        """
-        Returns the node with key k.
-        """
-        return self._get(self.root, k)
-
-    def _get(self, node, k):
-        if not node:
-            return None
-
-        if k < node.key:
-            return self._get(node.left, k)
-        elif k > node.key:
-            return self._get(node.right, k)
-        else:  # k == node.key
-            return node
-
-    def delete_min(self):
-        """
-        Delete the minimum element.
-        """
-        self.root = self._delete_min(self.root)
-
-    def _delete_min(self, node):
-        if not node.left:
-            return node.right
-
-        node.left = self._delete_min(node.left)
-        return node
-
-    def parent(self, k):
-        """
-        Returns parent of node with value k.
-        """
-        assert self.get(k)
-        if self.get(k) == self.root:
-            return None
-        return self._parent(self.root, k)
-
-    def _parent(self, node, k):
-        if not node:
-            return None
-        if node.left.key == k or node.right.key == k:
-            return node
-        else:
-            return self._parent(node.left, k) or self._parent(node.right, k)
-
-    def delete(self, k):
-        """
-        Recursive delete.
-        """
-        self.root = self._delete(self.root, k)
-
     def _delete(self, node, k):
+        """
+        Deletes a key from the subtree rooted at node.
+        """
         if not node:
             return None
 
@@ -197,4 +74,162 @@ class BST:
             if not (node.left or node.right):
                 return None
 
+            # one child
+            if not node.left:
+                return node.right
+            elif not node.right:
+                return node.left
+
+            # two children
+            succ = self._min(node.right)
+            succ.right = self._delete_min(node.right)
+            succ.left = node.left
+            return succ
+
         return node
+
+    def _delete_min(self, node):
+        """
+        Deletes the smallest key from the subtree rooted at node.
+        """
+        if not node.left:
+            return node.right
+
+        node.left = self._delete_min(node.left)
+        return node
+
+    def _floor(self, node, k):
+        """
+        Returns the largest key smaller than k from the subtree rooted at node.
+        """
+        if not node:
+            return None
+        elif k == node.key:
+            return k
+        elif k < node.key:
+            # key is smaller: continue left
+            return self._floor(node.left, k)
+        elif k > node.key:
+            # key is larger: floor(right) if one exists, else current
+            rf = self._floor(node.right, k)
+            return rf if rf else node.key
+
+    def _get(self, node, k):
+        if not node:
+            return None
+
+        if k < node.key:
+            return self._get(node.left, k)
+        elif k > node.key:
+            return self._get(node.right, k)
+        else:  # k == node.key
+            return node
+
+    def _height(self, node):
+        """
+        Returns the max height of the tree rooted at node.
+        """
+        if not node:
+            return 0
+
+        return 1 + max(self._height(node.left), self._height(node.right))
+
+    def _inorder(self, node, q):
+        """
+        Recursive in-order traversal.
+        """
+        if node:
+            self._inorder(node.left, q)
+            q.append(node.key)
+            self._inorder(node.right, q)
+
+        return q
+
+    def _min(self, node):
+        """
+        Returns the node containing the smallest key.
+        """
+        if not node:
+            return None
+
+        return self._min(node.left) or node
+
+    def _print_level(self, node, level):
+        if not node:
+            return ""
+        if level == 1:
+            return str(node.key) + " "
+        else:
+            left = self._print_level(node.left, level - 1)
+            right = self._print_level(node.right, level - 1)
+            return str(left) + str(right)
+
+    #-------------------------------------------------------------------------
+    # Public API
+    #-------------------------------------------------------------------------
+
+    def add(self, k):
+        """
+        Adds an entry to the bst instance.
+        """
+        self._add(k, self.root)
+
+    def ceil(self, k):
+        """
+        Returns the smallest key larger than k.
+        """
+        return self._ceil(self.root, k)
+
+    def delete(self, k):
+        """
+        Deletes a key k.
+        """
+        self.root = self._delete(self.root, k)
+
+    def delete_min(self):
+        """
+        Delete the minimum key.
+        """
+        self.root = self._delete_min(self.root)
+
+    def floor(self, k):
+        """
+        Returns the largest key smaller than k.
+        """
+        return self._floor(self.root, k)
+
+    def get(self, k):
+        """
+        Returns the node with key k.
+        """
+        return self._get(self.root, k)
+
+    def height(self):
+        """
+        Returns the max height.
+        """
+        return self._height(self.root)
+
+    def inorder(self):
+        """
+        in-order traversal
+        """
+        return self._inorder(self.root, [])
+
+    def level_order(self):
+        """
+        Level-order traversal (returns string).
+        """
+        maxh = self._height(self.root) + 1
+        levels = [self._print_level(self.root, level) for level in range(1, maxh)]
+        return "\n".join(levels)
+
+
+if __name__ == "__main__":
+    import random
+
+    nums = range(0, 100)
+    random.shuffle(nums)
+    bst = BST(nums[0])
+    [bst.add(i) for i in nums[1:]]
+    print bst.level_order()
--- test_bst.py
+++ test_bst.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 
 from bst import BST
@@ -10,8 +12,15 @@ def bst():
     return bst
 
 
-def test_inorder(bst):
-    assert bst.inorder() == range(1, 7)
+def test_inorder():
+    nums = range(1, 100)
+    random.shuffle(nums)
+    bst = BST(nums[0])
+    for n in nums[1:]:
+        bst.add(n)
+    nums.sort()
+
+    assert bst.inorder() == sorted(nums)
 
 
 def test_unique_queue(bst):
@@ -39,13 +48,6 @@ def test_get(bst):
         assert bst.get(i).key == i
 
 
-def test_parent(bst):
-    assert bst.parent(bst.root.key) is None
-    assert bst.parent(2) is bst.get(4)
-    assert bst.parent(6) is bst.get(4)
-    assert bst.parent(1) is bst.get(2)
-
-
 def test_height(bst):
     assert bst.height() == 3
 
@@ -60,7 +62,41 @@ def test_level_order(bst):
     assert bst.level_order() == "4 \n2 6 \n1 3 5 "
 
 
-def test_delete(bst):
+def test_delete_leaf(bst):
     for i in [5, 3, 1, 2, 6, 4]:
         bst.delete(i)
     assert bst.inorder() == []
+
+
+def test_delete_one_child(bst):
+    bst.delete(6)
+    assert bst.inorder() == [1, 2, 3, 4, 5]
+    assert bst.level_order() == "4 \n2 5 \n1 3 "
+
+
+def test_min(bst):
+    assert bst._min(bst.root).key == 1
+
+
+def test_delete_two_children(bst):
+    bst.delete(4)
+    assert bst.inorder() == [1, 2, 3, 5, 6]
+    bst.delete(5)
+    assert bst.inorder() == [1, 2, 3, 6]
+    bst.delete(1)
+    assert bst.inorder() == [2, 3, 6]
+
+
+def test_delete_random():
+    maxx = 200
+    nums = range(0, maxx)
+    random.shuffle(nums)
+    bst = BST(nums[0])
+    [bst.add(i) for i in nums[1:]]
+
+    random.shuffle(nums)
+    for i, v in enumerate(nums):
+        bst.delete(v)
+        remaining = bst.inorder()
+        assert v not in remaining
+        assert len(remaining) == maxx - (i + 1)
