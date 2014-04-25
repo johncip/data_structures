f78ed11814e0f850db0da384df4099fc5fbfcc11
John Cipriano <johnmikecip@gmail.com>
2014-04-24 20:16:21 -0700

    first shot at toposort

--- toposort.py
+++ toposort.py
@@ -8,26 +8,64 @@ TODO insert description
 from collections import defaultdict
 
 
-class DictGraph(object):
+def adjacent(edges):
     """
-    A multimap from vertex -> { connected vertices }.
+    Given [edges], returns a map: node -> {connected nodes}.
     """
+    res = defaultdict(set)
 
-    def __init__(self):
-        self.mm = defaultdict(set)
+    for v, w in edges:
+        res[v].add(w)
+        _ = res[w]
 
-    def add(self, v, w):
-        self.mm.get(w) # ensure it exists
-        self.mm[v].add(w)
+    return res
+
+
+def toposort(graph):
+    """
+    Returns a topological sort using DFS method.
+
+    @param graph    a map from node -> {connected nodes}
+    """
+    nodes = set(graph.keys())
+    marked = set()
+    temp = set()
+    res = []
+
+    def visit(n):
+        assert n not in temp, "must be acyclic"
+
+        if n not in marked:
+            temp.add(n)
+            for m in graph[n]:
+                visit(m)
+            marked.add(n)
+            res.append(n)
+            temp.remove(n)
+
+    while marked < nodes:
+        unmarked = iter(nodes - marked)
+        visit(next(unmarked))
+
+    return res[::-1]
 
 
 if __name__ == '__main__':
-    dg = DictGraph()
-    input = [
+    G = adjacent([
         (7, 11), (7, 8),
         (5, 11),
+        (8, 9),
         (3, 8), (3, 10),
-        (11, 2), (11, 9), (11, 10),
-    ]
+        (11, 2), (11, 9), (11, 10)])
+    print G
+    print toposort(G)
 
-    [dg.add(v, w) for v, w in input]
+    K = adjacent([
+        ('keep', 'on'),
+        ('keep', 'calm'),
+        ('calm', 'and'),
+        ('calm', 'carry'),
+        ('carry', 'on'),
+    ])
+    print K
+    print toposort(K)
