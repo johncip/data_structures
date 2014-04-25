18bd9754bedccc5240f6135ac457dc2ae62545dc
John Cipriano <johnmikecip@gmail.com>
2014-04-24 22:09:47 -0700

    finish topo

--- toposort.py
+++ toposort.py
@@ -1,33 +1,35 @@
 """
 toposort.py
 
-TODO insert description
+Topological sort using DFS method.
+
+Based on the Talentbuddy problem.
 
 """
 
 from collections import defaultdict
 
 
-def adjacent(edges):
+def adjacency_list(edges):
     """
-    Given [edges], returns a map: node -> {connected nodes}.
+    Given ["v,w"], returns a map: node -> {connected nodes}.
     """
-    res = defaultdict(set)
+    res = defaultdict(list)
 
-    for v, w in edges:
-        res[v].add(w)
-        _ = res[w]
+    for edge in edges:
+        v, w = edge.split(',')
+        res[v].append(w)
 
     return res
 
 
-def toposort(graph):
+def topological_sort(deps):
     """
-    Returns a topological sort using DFS method.
+    Returns a topological sort.
+    """
+    graph = adjacency_list(deps)
+    nodes = graph.keys()
 
-    @param graph    a map from node -> {connected nodes}
-    """
-    nodes = set(graph.keys())
     marked = set()
     temp = set()
     res = []
@@ -43,29 +45,16 @@ def toposort(graph):
             res.append(n)
             temp.remove(n)
 
-    while marked < nodes:
-        unmarked = iter(nodes - marked)
-        visit(next(unmarked))
+    for n in nodes:
+        visit(n)
 
     return res[::-1]
 
 
 if __name__ == '__main__':
-    G = adjacent([
-        (7, 11), (7, 8),
-        (5, 11),
-        (8, 9),
-        (3, 8), (3, 10),
-        (11, 2), (11, 9), (11, 10)])
-    print G
-    print toposort(G)
+    deps = ['7,11', '7,8', '5,11', '8,9', '3,8', '3,10', '11,2', '11,9',
+            '11,10']
+    print topological_sort(deps)
 
-    K = adjacent([
-        ('keep', 'on'),
-        ('keep', 'calm'),
-        ('calm', 'and'),
-        ('calm', 'carry'),
-        ('carry', 'on'),
-    ])
-    print K
-    print toposort(K)
+    deps = ["keep,on", "keep,calm", "calm,and", "calm,carry", "carry,on"]
+    print topological_sort(deps)
